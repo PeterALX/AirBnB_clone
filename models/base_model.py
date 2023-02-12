dfcb076c830e9451759b53ec4e3d215ba1c6060a
PeterALX <quaypeter25@gmail.com>
2023-02-12 08:18:14 +0000

    first commit

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" class BaseModel defined here """
+import uuid
+from datetime import datetime 
+# from models import storage
+import models
+
+class BaseModel:
+    """ class BaseModel is the base class for all other models, defining common functionalities save, __str__, save and to_dict"""
+    def __init__(self, *args, **kwargs):
+        """ initialization of instance """
+        if (kwargs):
+            if("id" not in kwargs or "created_at" not in kwargs or "updated_at" not in kwargs):
+                #raise them errors.
+                print("raise an error here: either created_at or updated_at doesn't exist ")
+                return
+            for (key, value) in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at # potential bug, as the assignment seems to require separate new date here
+            models.storage.new(self)
+
+    def __str__(self):
+        """ returns a string represantation of class """
+        return(f"[{self.__class__.__name__}] ({self.id}){self.__dict__}")
+
+    def save(self):
+        """ updates a BaseModel instance's updated_at property and saves changes to disk when called"""
+        self.updated_at = datetime.now() 
+        models.storage.save()
+
+    def to_dict(self):
+        """ Returns a dictionary represantion of the class. This dictionary is used for serialization into json for storage in disk """
+        my_dict = dict(self.__dict__ ) #potential bugs here, consider doing a deepcopy instead
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        my_dict["__class__"] = self.__class__.__name__
+        return my_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" class Filestorage """
+import json
+from models.base_model import BaseModel
+
+class FileStorage:
+    """ 
+    This class handles storage:
+            serialisation of models to json to store into disk,
+            reloading data from disk on app startup
+            keeping disk storage up to date with the apps state
+    """
+    __file_path = "database.json"
+    __objects = {}
+
+    def __init__(self):
+        """no initialization to be done """
+        pass
+    def all(self):
+        """returns __objects"""
+        return self.__objects
+    def new(self,obj):
+        """ adds a model to storage. ie __objects """
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+    def save(self):
+        """saves __objects to disk """
+        dict_objects = dict(self.__objects)
+        for key in dict_objects.keys():
+            dict_objects[key] = dict_objects[key].to_dict()
+        jayson= json.dumps(dict_objects, indent=8) # probably remove this indent for checker
+        with open(self.__file_path, "w") as file:
+            file.write(jayson)
+
+
+    def reload(self):
+        """ reloads data from disk when app is initialized """
+        class_list = {
+                "BaseModel":BaseModel
+        }
+        try:
+            with open(self.__file_path, "r") as file:
+                json_string = file.read() #read the json into an empty string first because apparently json.dumping an empty file throws an error
+                if len(json_string) == 0:
+                    return
+                dict_objects = json.loads(json_string)
+                for (key, value) in dict_objects.items():
+                    self.__objects[key] = class_list[value["__class__"]](**value)
+        except FileNotFoundError:
+            pass
