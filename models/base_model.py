ad9cd0dae628e01827d25ca0ffc2e6af62837d43
PeterALX <quaypeter25@gmail.com>
2024-03-25 08:55:46 +0000

    some fixes

--- models/base_model.py
+++ models/base_model.py
@@ -16,10 +16,10 @@ class BaseModel:
                         v, "%Y-%m-%dT%H:%M:%S.%f"))
                 elif k != '__class_':
                     setattr(self, k, v)
-                else:
-                    self.id = str(uuid.uuid4())
-                    self.created_at = datetime.now()
-                    self.updated_at = self.created_at
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
