ac2bbf0f2e07af9b824f45103cac19158c1a232b
Peter ALX <quaypeter25@gmail.com>
2024-08-13 09:28:32 +0000

    write a bit better tests for basemodel and filestorage

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,39 +1,62 @@
 #!/usr/bin/python3
-import unittest
+"""tests for base_model"""
 from models.base_model import BaseModel
+from models import storage
+import unittest
+from datetime import datetime
+import os
 import json
 
 
-# think about them tests
 class TestBaseModel(unittest.TestCase):
-    def setUp(self):
-        self.base = BaseModel()
-
-    def tearDown(self):
-        del self.base
+    # def setUp(self):
+    #     """ set/reset test environment for each test """
+    #     storage._FileStorage__file_path = 'test_db.json'
+    #     storage._FileStorage__objects = {}
+    #
+    # def tearDown(self):
+    #     """ clear test environment after every test """
+    #     if os.path.exists(storage._FileStorage__file_path):
+    #         os.remove(storage._FileStorage__file_path)
 
     def test_init(self):
-        self.assertIsInstance(self.base, BaseModel)
+        """ testing the model constructor"""
+        original = BaseModel()
+        original_dict = original.to_dict()
+        copy = BaseModel(**original_dict)
+        copy_dict = copy.to_dict()
+        self.assertEqual(original_dict, copy_dict)
 
-    def test_kwargs_init(self):
-        model = BaseModel()
-        model_dict = model.to_dict()
-        model_clone = BaseModel(**model_dict)
-        self.assertFalse(model_clone is model)
-
-    def test_to_dict(self):
-        d = self.base.to_dict()
-        self.assertIsInstance(d['created_at'], str)
-        self.assertIsInstance(d['updated_at'], str)
-        self.assertIsInstance(d['id'], str)
+        # test that a new instance is stored/tracked by storage engine
+        self.assertIs(storage.all()[f'BaseModel.{original.id}'], original)
 
     def test_str(self):
-        string_rep = f'[BaseModel] ({self.base.id}) {self.base.__dict__}'
-        self.assertEqual(str(self.base), string_rep)
+        """ testing __str__ """
+        model = BaseModel()
+        string_rep = f'[BaseModel] ({model.id}) {model.__dict__}'
+        self.assertEqual(str(model), string_rep)
+
+    def test_to_dict(self):  # TODO!!!
+        """ testing to_dict"""
+        model = BaseModel()
+        d = model.to_dict()
+        self.assertEqual(d['__class__'], model.__class__.__name__)
+        for k, v in model.__dict__.items():
+            message = f'property \'{k}\' missing from \
+dictionary representation of BaseModel'
+            self.assertIn(k, d, msg=message)
+            if type(v) is datetime:
+                self.assertIsInstance(d[k], str)
+                self.assertIsInstance(datetime.fromisoformat(d[k]), datetime)
 
     def test_save(self):
-        self.base.save()
-        key = 'BaseModel' + "." + self.base.id
-        with open('db.json', 'r') as f:
-            j = json.load(f)
-            self.assertEqual(j[key], self.base.to_dict())
+        """ testing BaseModel's save """
+        model = BaseModel()
+        model.save()
+        self.assertNotEqual(model.created_at, model.updated_at)
+
+        # test that model is saved to json properly
+        d = model.to_dict()
+        with open(storage._FileStorage__file_path, 'r') as f:
+            json_dict = json.load(f)
+            self.assertEqual(json_dict[f'BaseModel.{model.id}'], d)
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,2 +1,60 @@
 #!/usr/bin/python3
+"""tests for the file storage engine"""
+from models import storage
+from models.base_model import BaseModel
+import unittest
+import os
+import json
 
+
+class TestFileStorage(unittest.TestCase):
+    # def setUp(self):
+    #     """ set/reset test environment for each test """
+    #     storage._FileStorage__file_path = 'test_db.json'
+    #     storage._FileStorage__objects = {}
+    #
+    # def tearDown(self):
+    #     """ clear test environment after every test """
+    #     if os.path.exists(storage._FileStorage__file_path):
+    #         os.remove(storage._FileStorage__file_path)
+
+    def test_new(self):
+        """ test new object addition on instantiation """
+        model = BaseModel()
+        self.assertIs(storage.all()[f'BaseModel.{model.id}'], model)
+
+    def test_save(self):
+        """ test that storage.save serializes models to json properly """
+        model = BaseModel()
+        d = model.to_dict()
+        storage.save()
+        with open(storage._FileStorage__file_path, 'r') as f:
+            json_dict = json.load(f)
+            self.assertEqual(json_dict[f'BaseModel.{model.id}'], d)
+
+    def test_reload(self):
+        """ test that storage.save serializes models to json properly """
+        model = BaseModel()
+        d = model.to_dict()
+        storage.save()
+        len_before_reload = len(storage.all())
+        storage.reload()
+        len_after_reload = len(storage.all())
+
+        reloaded_model = storage.all()[f'BaseModel.{model.id}']
+        self.assertIsNot(reloaded_model, model)
+        self.assertEqual(reloaded_model.to_dict(), d)
+
+        self.assertEqual(len_before_reload, len_after_reload)
+
+    def test_filepath(self):
+        """
+        Test that storage engine's file path is present and a string
+        """
+        self.assertIsInstance(storage._FileStorage__file_path, str)
+
+    def test_objects(self):
+        """
+        Test that storage engine's __objects is present and a dict
+        """
+        self.assertIsInstance(storage._FileStorage__objects, dict)
